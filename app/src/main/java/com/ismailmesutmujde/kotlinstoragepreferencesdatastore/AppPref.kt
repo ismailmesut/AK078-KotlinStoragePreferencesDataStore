ba82a2b28a53a6958066c37e6933eb44819a6b4d
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-21 23:05:05 +0300

    updated: different types of records

--- app/src/main/java/com/ismailmesutmujde/kotlinstoragepreferencesdatastore/AppPref.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoragepreferencesdatastore/AppPref.kt
@@ -3,8 +3,12 @@ package com.ismailmesutmujde.kotlinstoragepreferencesdatastore
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.doublePreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.stringSetPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
 
@@ -14,6 +18,10 @@ class AppPref(var context : Context) {
 
     companion object {
         val NAME_KEY = stringPreferencesKey("NAME")
+        val AGE_KEY = intPreferencesKey("AGE")
+        val HEIGHT_KEY = doublePreferencesKey("HEIGHT")
+        val AREYOUSINGLE_KEY = booleanPreferencesKey("AREYOUSINGLE")
+        val FRIEND_LIST_KEY = stringSetPreferencesKey("FRIEND_LIST")
     }
 
     suspend fun recordName(name: String) {
@@ -30,6 +38,74 @@ class AppPref(var context : Context) {
     suspend fun deleteName() {
         context.dataStore.edit {
             it.remove(NAME_KEY)
+        }
+    }
+
+    suspend fun recordAge(age: Int) {
+        context.dataStore.edit {
+            it[AGE_KEY] = age
+        }
+    }
+
+    suspend fun readAge() : Int {
+        val p = context.dataStore.data.first()
+        return p[AGE_KEY] ?: 0
+    }
+
+    suspend fun deleteAge() {
+        context.dataStore.edit {
+            it.remove(AGE_KEY)
+        }
+    }
+
+    suspend fun recordHeight(height: Double) {
+        context.dataStore.edit {
+            it[HEIGHT_KEY] = height
+        }
+    }
+
+    suspend fun readHeight() : Double {
+        val p = context.dataStore.data.first()
+        return p[HEIGHT_KEY] ?: 0.0
+    }
+
+    suspend fun deleteHeight() {
+        context.dataStore.edit {
+            it.remove(HEIGHT_KEY)
+        }
+    }
+
+    suspend fun recordAreYouSingle(areYouSingle: Boolean) {
+        context.dataStore.edit {
+            it[AREYOUSINGLE_KEY] = areYouSingle
+        }
+    }
+
+    suspend fun readAreYouSingle() : Boolean {
+        val p = context.dataStore.data.first()
+        return p[AREYOUSINGLE_KEY] ?: false
+    }
+
+    suspend fun deleteAreYouSingle() {
+        context.dataStore.edit {
+            it.remove(AREYOUSINGLE_KEY)
+        }
+    }
+
+    suspend fun recordFriendList(friendList: Set<String>) {
+        context.dataStore.edit {
+            it[FRIEND_LIST_KEY] = friendList
+        }
+    }
+
+    suspend fun readFriendList() : Set<String>? {
+        val p = context.dataStore.data.first()
+        return p[FRIEND_LIST_KEY]
+    }
+
+    suspend fun deleteFriendList() {
+        context.dataStore.edit {
+            it.remove(FRIEND_LIST_KEY)
         }
     }
 }
--- app/src/main/java/com/ismailmesutmujde/kotlinstoragepreferencesdatastore/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinstoragepreferencesdatastore/MainActivity.kt
@@ -17,10 +17,40 @@ class MainActivity : AppCompatActivity() {
         val appPref = AppPref(this)
 
         val job = CoroutineScope(Dispatchers.Main).launch {
-            //appPref.recordName("Ahmet")
-            appPref.deleteName()
+            appPref.recordName("Ahmet")
+            appPref.recordAge(23)
+            appPref.recordHeight(1.78)
+            appPref.recordAreYouSingle(true)
+
+            val friendList = HashSet<String>()
+            friendList.add("Mehmet")
+            friendList.add("Zeynep")
+            appPref.recordFriendList(friendList)
+
+
+            //appPref.deleteName()
+            //appPref.deleteAge()
+            //appPref.deleteHeight()
+            //appPref.deleteAreYouSingle()
+            //appPref.deleteFriendList()
+
             val incomingName = appPref.readName()
+            val incomingAge = appPref.readAge()
+            val incomingHeight = appPref.readHeight()
+            val incomingAreYouSingle = appPref.readAreYouSingle()
+
             Log.e("Incoming Name", incomingName)
+            Log.e("Incoming Age", incomingAge.toString())
+            Log.e("Incoming Height", incomingHeight.toString())
+            Log.e("Incoming AreYouSingle", incomingAreYouSingle.toString())
+
+            val incomingFriendList = appPref.readFriendList()
+            if (incomingFriendList != null) {
+                for (f in incomingFriendList) {
+                    Log.e("Incoming Friend", f)
+                }
+            }
+
         }
     }
 }
